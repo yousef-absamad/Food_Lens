9d398bf6509251980afa9fae3c7e1d923fca0089
Yousef Abdel Samad <yousef.ahmed.0402@gmail.com>
2025-06-03 11:21:01 +0300

    add LauncherScreen to handle onboarding and auth state and Removed auth decision logic from main.dart

---
+++ lib/launcher_screen.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:food_lens/features/auth/screens/login_screen.dart';
+import 'package:food_lens/features/home/home.dart';
+import 'package:food_lens/features/intro/onboard/screens/onboard_screen.dart';
+import 'package:food_lens/features/intro/onboard/services/on_boarding_services.dart';
+
+class LauncherScreen extends StatefulWidget {
+  const LauncherScreen({super.key});
+
+  @override
+  State<LauncherScreen> createState() => _LauncherScreenState();
+}
+
+class _LauncherScreenState extends State<LauncherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkAndNavigate();
+    });
+  }
+
+ Future<void> _checkAndNavigate() async {
+  final isFirstTime = await OnBoardingServices.isFirstTime();
+  
+  if (!mounted) return;
+
+  if (isFirstTime) {
+    OnBoardingServices.setFirstTimeWithFalse();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const OnboardScreens()),
+    );
+  } else {
+    final user = FirebaseAuth.instance.currentUser;
+    if (!mounted) return;
+
+    if (user == null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const Home()),
+      );
+    }
+  }
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,32 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_lens/features/auth/emailVerification/views/email_verification_screen.dart';
-import 'package:food_lens/features/auth/screens/login_screen.dart';
-import 'package:food_lens/features/home/screens/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:food_lens/launcher_screen.dart';
+import 'package:food_lens/features/intro/onboard/services/on_boarding_services.dart';
 import 'package:food_lens/firebase_options.dart';
-import 'package:food_lens/features/intro/splash_screen.dart';
-
-import 'features/home/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await OnBoardingServices.initializeSharedPrefrencesStorge();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const FoodLensApp());
 }
 
-class FoodLensApp extends StatefulWidget {
+
+class FoodLensApp extends StatelessWidget {
   const FoodLensApp({super.key});
 
-  @override
-  State<FoodLensApp> createState() => _FoodLensAppState();
-}
-
-class _FoodLensAppState extends State<FoodLensApp> {
-  @override
-  void initState() {
-    super.initState();
-    FirebaseAuth.instance.idTokenChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-      } else {
-        print('User is signed in!');
-      }
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Food Lens',
-      home: SplashScreen(),
-      //home: LoginScreen(),
-      //home: EmailVerificationScreen(),
+      home: const LauncherScreen(),
     );
   }
 }
